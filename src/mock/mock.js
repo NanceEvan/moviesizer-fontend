3c6eab76c7c0dd98b8eba5ae94a4109642b9c991
sizer <591207060@qq.com>
2019-04-11 10:54:50 +0800

    搜索框下移

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -2,6 +2,7 @@ const Mock = require('mockjs') // 获取mock对象
 // const Random = Mock.Random // 获取random对象，随机生成各种数据，具体请翻阅文档
 // const code = 200 // 返回的状态码
 
+// Banners 数据
 const postBanners = req => {
   return require('./mocker/banners')
 }
